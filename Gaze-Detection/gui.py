4a35f4389e7639c65d3fced71861583ce7afe0db
gdawe18 <cdesimone18@stjohnsprep.org>
2018-02-06 11:52:52 -0500

    Indentation

--- Gaze-Detection/gui.py
+++ Gaze-Detection/gui.py
@@ -17,7 +17,7 @@ class VideoWindow(QMainWindow):
         super(VideoWindow, self).__init__(parent)
         self.camera = camera
         self.camera.iniCamera()
-    	self.camera.startVid()
+        self.camera.startVid()
         # quit on alt+f4 or ctrl+w
         self.shortcut = QShortcut(QKeySequence.Close, self)
         self.shortcut.activated.connect(self.onQuit)
