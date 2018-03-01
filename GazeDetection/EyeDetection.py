b9c366431fab616010d34e93bad518d79fae816f
Noodleham <l77587481@gmail.com>
2018-03-01 12:12:15 -0500

    Draws eye rectanges in GUI.py

--- GazeDetection/EyeDetection.py
+++ GazeDetection/EyeDetection.py
@@ -22,18 +22,21 @@ class Eye:
         self.height = height
 
 
-def detect_eyes(img):
+def detect_eyes(img, draw_rects=False):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.2, 5)
     eyes_rect = []
     for (x, y, w, h) in faces[:MAX_FACES]:
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        if(draw_rects):
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
-        # roi_color = img[y:y + h, x:x + w]
+        if (draw_rects):
+            roi_color = img[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 4)
         for (ex, ey, ew, eh) in eyes[:MAX_EYES_PER_FACE]:
             eyes_rect.append(Eye(ex + x, ey + y, ew, eh))
-            # cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            if(draw_rects):
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     return img, eyes_rect
 
 
