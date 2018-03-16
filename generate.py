d7ffe0d451f3ac7056ff358fe10b9ed5cc8553a8
spec <sp3c-sec@protonmail.com>
2018-03-16 12:40:20 -0400

    Completed multiprocessing in training_generation

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("gaze_path", help="Full path of the Columbia Gaze Data Set")
+parser.add_argument("output_file", help="Training Data Output File Path")
 args = parser.parse_args()
-create_training_data(args.gaze_path)
+create_training_data(args.gaze_path, args.output_file)
 
